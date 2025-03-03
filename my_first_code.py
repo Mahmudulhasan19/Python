5dc7d6c795e3fda93b9b5e5129923419331fd018
Mahmudulhasan19 <mahmudulhasanmatin8@gmail.com>
2025-03-03 17:35:11 +0600

    python

---
+++ my_first_code.py
@@ -0,0 +1,41 @@
+# print("Matin")
+# print(5)
+# name = "matin "
+# age = 12
+# address = "ataullah"
+# old= False
+# a= None
+
+# print("Name: ", name)
+# print("Address:", address)
+
+'''print(type(age))
+print(type(name))
+print(type(old))
+print(type(a))'''
+
+'''import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))'''  # To count the total number of keywords
+
+
+'''print(name)
+print(address)'''
+# print(age)
+
+
+# #Slicing
+# str = "Mahmudul Hasan"
+# print(str[:3])
+
+# #some function
+# print(str.endswith("san"))
+
+#Practise question
+
+#print("Enter your Name:")
+name = input("Enter Your Name:")
+#print("Name:",name)
+print(len(name))
+print(name.find("s"))
+print(name.count("h"))

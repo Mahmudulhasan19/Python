c33f55e349ce0fb43904e048212f96a4fbaa9319
Mahmudulhasan19 <mahmudulhasanmatin8@gmail.com>
2025-03-05 19:38:54 +0600

    dictionary and list

---
+++ dictionary_set.py
@@ -0,0 +1,22 @@
+matin = {
+    "name": "mahmudul",
+    "ID": "IT-2101919",
+    "subject": ["Python", "Java", "C++"],  # list , also we can write tuple
+
+}
+# Print dictionary and type
+print(type(matin))
+
+# Access in key of dictionary
+#print(matin["name"])
+
+#Change the key value(Mutable)
+""" matin["name"] = "Hasan"
+matin["ID"] = "IT-21020"
+print(matin) """
+
+#Create null dictionary
+null_dict = {}
+print(null_dict)
+
+

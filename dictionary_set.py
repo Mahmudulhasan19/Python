c54111844df224c6297defd3471809ddb74e803f
Mahmudulhasan19 <mahmudulhasanmatin8@gmail.com>
2025-03-06 23:04:50 +0600

    Update dictionary part

--- dictionary_set.py
+++ dictionary_set.py
@@ -1,4 +1,6 @@
-matin = {
+
+# DiCTIONARY
+""" matin = {
     "name": "mahmudul",
     "ID": "IT-2101919",
     "subject": ["Python", "Java", "C++"],  # list , also we can write tuple
@@ -8,15 +10,43 @@ matin = {
 print(type(matin))
 
 # Access in key of dictionary
-#print(matin["name"])
+# print(matin["name"])
 
-#Change the key value(Mutable)
-""" matin["name"] = "Hasan"
+# Change the key value(Mutable)
+matin["name"] = "Hasan"
 matin["ID"] = "IT-21020"
 print(matin) """
 
-#Create null dictionary
+
+""" # Create null dictionary
 null_dict = {}
 print(null_dict)
+ """
 
+# Nested Dictionary
+""" info = {
+    "name" : "matin",
+    "subject": {
+        "Chem" : 97,
+        "math" : 100,
+        "Phy"  : 67,
+        "Eng"  : 89,
+    },                #Careful here, here also has a comma
+    "ID" : "IT-21019"
+} """
 
+""" print(info)
+#Access specific key
+print(info["subject"] ["Chem"])
+ """
+# Dictionary Method
+""" print(info.keys())
+print(info.values())
+print(info["name"])       # If error, then next line could not be execute
+print(info.get("name"))  #Method is preferable for error handling, Return none
+new_dict  ={
+    "city":"Dhaka",
+}
+info.update(new_dict)   
+print(info)
+ """
